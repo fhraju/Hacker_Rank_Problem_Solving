219d1a57ab5cfe47c32ec707d05a0bde4fdc6f82
fhraju <alonghuman@gmail.com>
2022-03-21 16:06:25 +0600

    create problem_1_sum.py

---
+++ Problem_1_sum.py
@@ -0,0 +1,10 @@
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    sum = a + b
+    return sum
+
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+res = solveMeFirst(num1,num2)
+print(res)
