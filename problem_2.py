f3b8c7125933fc8033ef4e95fa6af4edf8a90970
fhraju <alonghuman@gmail.com>
2022-03-23 22:53:47 +0600

    Update problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -1,8 +1,9 @@
-#!/bin/python
+#!/bin/python3
 
 import math
 import os
 import random
+import re
 import sys
 
 #
@@ -14,9 +15,15 @@ import sys
 
 def simpleArraySum(ar):
     # Write your code here
-    sum = 0
-    for i in ar[0:]:
-        sum = sum+i
-    return sum
 
-print(simpleArraySum(ar = list(input())))
+    if __name__ == '__main__':
+        fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+        ar_count = int(input().strip())
+
+        ar = list(map(int, input().rstrip().split()))
+
+    
+    fptr.write(str(result) + '\n')
+    ptr.close()
+    result = simpleArraySum(ar)
